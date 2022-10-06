47428f7d3943888159faddef529de7d8171a5f3f
Kyanh Pham <kyanh.pham@mths.ca>
2022-10-06 14:48:08 +0000

    calculates the volume of a rectangular prism

---
+++ volumeOfRectangularPrism.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Kyanh Pham
+//
+// Created by: Kyanh Pham
+// Created on: Oct 2022
+// This program calculates the volume of a rectangular prism
+// with length, width and height inputted from user
+
+#include <iostream>
+
+int main() {
+    // This function calculates volume
+    int length;
+    int width;
+    int height;
+    int volume;
+
+    // Input
+    std::cout << "Enter the length (cm): ";
+    std::cin >> length;
+    std::cout << "Enter the width (cm): ";
+    std::cin >> width;
+    std::cout << "Enter the height (cm) ";
+    std::cin >> height;
+
+    // Process
+    volume = length * width * height;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Volume is " << volume << " cm³." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
